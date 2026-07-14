e3be01307cdb728521bc9965e29ea1f9c3eddd35
Jim_Motsch <jimmymotsch@gmail.com>
2026-07-13 20:06:24 -0500

    exercise 4: extract filter pipeline with system prompt

--- ex3.ts
+++ ex3.ts
@@ -2,11 +2,9 @@ import * as z from "zod";
 import { anthropic } from "@ai-sdk/anthropic"
 import { generateObject } from "ai"
 const sentences = [
-  "I hate the heat, thinking about moving to Portland", //city portalnd temp: cold
-  "Chicago winters are brutal but honestly I love the cold", //city chicago temp cold
-  "I could really go for some warm weather right about now", // city null temp warm
-  "Just got back from visiting my sister in Denver", // city denver temp null
-  "My chromebook won't turn on and I have homework due",
+  "dell laptops at Lincoln from 2022",
+  "ipads older than 2021",
+  "everything at the highschool"
 ];
 const temp = ["hot", "cold"]
 const tempEnum = z.enum(temp)
@@ -14,11 +12,17 @@ const schema = z.object({
     city: z.string(),
     tempPreference: tempEnum.nullable()
 })
+const schema2 = z.object({
+    school: z.string().nullable(),
+    model: z.string().nullable(),
+    yearMin: z.number().nullable(),
+    yearMax: z.number().nullable()
+})
 for (const q of sentences){
 const {object} = await generateObject({
     model: anthropic('claude-haiku-4-5'),
     prompt: q,
-    schema: schema
+    schema: schema2
 });
 console.log(object);
 }
